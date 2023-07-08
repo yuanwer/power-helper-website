b6ca072d03f9ed0498a39fce1d75acff8a4d2152
yuanwer <jw_22@139.com>
2023-07-08 18:05:55 +0800

    Initial commit

---
+++ assets/hero.93bdb9d4.js
@@ -0,0 +1,1 @@
+import{I as r}from"./index-798fa2fe.js";function t(){return r({url:"https://www.sapi.run/hero/getHeroList.php"})}function e(t){return r({url:`https://www.sapi.run/hero/select.php?hero=${t.hero}&type=${t.type}`})}export{e as a,t as g};
---
+++ assets/uni-app.es.92259da7.js
@@ -0,0 +1,1 @@
+import{a1 as o}from"./index-798fa2fe.js";const r=(o,r)=>{const t=o.__vccOpts||o;for(const[n,s]of r)t[n]=s;return t};function t(r,t){return o(r)?t:r}export{r as _,t as r};
